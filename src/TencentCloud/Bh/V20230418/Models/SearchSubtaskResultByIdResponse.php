c9ee4dff490566bb5c4c38c2fbff5c97d78e1f24
tencentcloudapi <tencentcloudapi@tencent.com>
2026-02-09 10:34:58 +0800

    release 3.0.1537

--- src/TencentCloud/Bh/V20230418/Models/SearchSubtaskResultByIdResponse.php
+++ src/TencentCloud/Bh/V20230418/Models/SearchSubtaskResultByIdResponse.php
@@ -22,6 +22,8 @@ use TencentCloud\Common\AbstractModel;
  *
  * @method integer getTotalCount() 获取记录数
  * @method void setTotalCount(integer $TotalCount) 设置记录数
+ * @method array getSubtaskResult() 获取运维子任务执行结果
+ * @method void setSubtaskResult(array $SubtaskResult) 设置运维子任务执行结果
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
@@ -33,12 +35,18 @@ class SearchSubtaskResultByIdResponse extends AbstractModel
     public $TotalCount;
 
     /**
+     * @var array 运维子任务执行结果
+     */
+    public $SubtaskResult;
+
+    /**
      * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
      * @param integer $TotalCount 记录数
+     * @param array $SubtaskResult 运维子任务执行结果
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -56,6 +64,15 @@ class SearchSubtaskResultByIdResponse extends AbstractModel
         }
         if (array_key_exists("TotalCount",$param) and $param["TotalCount"] !== null) {
             $this->TotalCount = $param["TotalCount"];
+        }
+
+        if (array_key_exists("SubtaskResult",$param) and $param["SubtaskResult"] !== null) {
+            $this->SubtaskResult = [];
+            foreach ($param["SubtaskResult"] as $key => $value){
+                $obj = new SubtaskResult();
+                $obj->deserialize($value);
+                array_push($this->SubtaskResult, $obj);
+            }
         }
 
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
